134985c5116c7d83d5a735319ab30d3ba64bca6e
mewtoex <whandersonba75@gmail.com>
2024-12-15 20:28:57 -0300

    iniciando o projeto

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import os
+from tkinter import Tk, Button, filedialog, Label
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 
@@ -26,11 +28,28 @@ def processar_xlsx(arquivo):
 
 
                     if prev_array[1] != current_array[1] and prev_array[2] != current_array[2]:
-                        print("Vermelho")
                         for cell_to_color in row:
                             cell_to_color.fill = fill_red
                         break
 
-    wb.save("Weight (1)_processado.xlsx")
+    nome_arquivo, extensao = os.path.splitext(arquivo)
+    novo_arquivo = f"{nome_arquivo}_processado{extensao}"
+    wb.save(novo_arquivo)
+    return novo_arquivo
 
-processar_xlsx("Weight (1).xlsx")
+def selecionar_arquivo():
+    caminho_arquivo = filedialog.askopenfilename(filetypes=[("Arquivos Excel", "*.xlsx")])
+    if caminho_arquivo:
+        arquivo_processado = processar_xlsx(caminho_arquivo)
+        label_resultado.config(text=f"Arquivo salvo: {arquivo_processado}")
+
+janela = Tk()
+janela.title("Processador de Arquivo XLSX")
+
+botao_selecionar = Button(janela, text="Selecionar Arquivo XLSX", command=selecionar_arquivo)
+botao_selecionar.pack(pady=10)
+
+label_resultado = Label(janela, text="")
+label_resultado.pack(pady=10)
+
+janela.mainloop()
